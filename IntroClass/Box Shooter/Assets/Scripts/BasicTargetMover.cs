2f012f72c720f3cd30a0d37dbc4480300955b9ab
Wesley Matlock <wes@insoc.net>
2015-10-16 08:45:30 -0500

    Initial add.

---
+++ IntroClass/Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+
+	public bool doSpin = true;
+	public float spinSpeed = 180.0f;
+
+	public bool doMotion = false;
+	public float motionMagnitude = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+	
+		// rotate around the UP axis of the gameObject.
+		if (doSpin) {
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+		}
+
+		// move up and down over time. 
+		if (doMotion) {
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagnitude);
+		}
+
+	}
+}
